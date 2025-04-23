c3c40d60c137772baa1dbc2080ea8a1135265a39
thanhnd <ducthanhnguyen95@gmail.com>
2025-04-23 22:09:37 +0700

    303. Range Sum Query - Immutable

---
+++ src/com/codewiththanh/datastructure/Main.java
@@ -0,0 +1,9 @@
+package com.codewiththanh.datastructure;
+
+public class Main {
+    public static void main(String[] args) {
+        var numArray = new NumArray(new int[]{-2,0,3,-5,2,-1});
+        System.out.println(numArray.sumRange(2, 5));
+    }
+
+}
